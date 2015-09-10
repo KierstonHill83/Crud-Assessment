be3a8dfc37e6e6b6b72e891c3e0e3aeba29ce351
KierstonHill83 <khill1218@gmail.com>
2015-09-10 08:57:37 -0600

    Changed url

--- server/models/superhero.js
+++ server/models/superhero.js
@@ -9,6 +9,7 @@ var Superhero = new Schema(
   }
 );
 
-mongoose.connect('mongodb://localhost/node-superheros');
+process.env.DB_HOST = "mongodb://heroku_hvxk1pzn:1q5nft62jsikd4qc1stdlg4sc9@ds041571.mongolab.com:41571/heroku_hvxk1pzn";
+mongoose.connect(process.env.MONGOLAB_URI || "mongodb://localhost/node-superhero");
 
 module.exports = mongoose.model('superheros', Superhero);
